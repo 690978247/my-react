60f9695b8ebc8730577f31ea0941c34e9ce36e67
‘xu’ <laike.xu@shengyc.com>
2021-07-01 17:31:44 +0800

    add ant-design

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Header from '../../components/header'
 import './home.scss'
+import { Button } from 'antd'
 
 class Home extends Component {
   render() {
@@ -8,6 +9,7 @@ class Home extends Component {
       <div className="P-home">
         <Header param1='abc' param2='c' func1={() => {console.log('func1')}} />
         <h1>Home Page</h1>
+        <Button type="primary">Button</Button>
       </div>
     )
   }
