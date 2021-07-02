d4eb970e145e337685cc7060251abbdcb04c7dee
‘xu’ <laike.xu@shengyc.com>
2021-07-02 18:18:00 +0800

    update

--- src/pages/iot/index.js
+++ src/pages/iot/index.js
@@ -1,13 +1,108 @@
 import React, { Component } from 'react' 
 import './iot.scss'
-import { Table } from 'antd'
+import { Table, Input, Button, Modal } from 'antd'
 
 class IOT extends Component  {
+  constructor(props){
+    super(props)
+    this.state = {
+      popup: false, 
+      cloums: [
+        {
+          title: '序号',
+          dataIndex: 'num',
+        },
+        {
+          title: '变量名',
+          dataIndex: 'name',
+        },
+        {
+          title: '变量描述',
+          dataIndex: 'describe',
+        },
+        {
+          title: '数据类型',
+          dataIndex: 'dataType',
+        },
+        {
+          title: '变量地址',
+          dataIndex: 'address',
+          render: address => (
+            <div className="address-text">
+              <Input className="address-input" />
+              <Button className="address-btn" onClick={() => {this.poenPop()}} >. . .</Button>
+            </div>
+          )
+        },
+        {
+          title: '字符长度',
+          dataIndex: 'codeLen',
+          render: (codeLen) => (
+            <div className="address-text">
+              <Input className="address-input" value={codeLen} />
+            </div>
+          )
+        },
+        {
+          title: '缩放比',
+          dataIndex: 'zoom',
+        },
+        {
+          title: '数据归档',
+          dataIndex: 'dataFile',
+        },
+        
+      ],
+      tableData: [
+        {
+          key: '1',
+          num: '1',
+          name: '',
+          describe: '',
+          dataType: '二进制变量',
+          address: '',
+          codeLen: '4',
+          zoom: '',
+          dataFile: ''
+        },
+      ]
+    }
+  }
+
+  poenPop() {
+    this.setState({
+      popup: true
+    })
+  }
+
+  confirmPop() {
+    this.setState({
+      popup: false
+    })
+  }
+
+  cancelPop() {
+    this.setState({
+      popup: false
+    })
+  }
+
   render() {
     return (
-      <Table>
-
-      </Table>
+      <>
+        <Table 
+          columns={this.state.cloums}
+          dataSource={this.state.tableData}
+          pagination={false}
+          bordered
+        />
+        <Modal 
+          title="选择OPC变量"
+          visible={this.state.popup}
+          onOk={() => {this.confirmPop()}}
+          onCancel={() => {this.cancelPop()}}
+        />
+      </>
     )
   }
 }
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,10 @@
-const proxy = require('http-proxy-middleware')
-module.exports = function(app) {
-  app.use(
-    '^/api',
-    proxy({
-      target: 'http:/localhost',
-      changeOrigin: true
-    })
-  )
-}
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+
+    app.use(createProxyMiddleware('^/api',{
+        target:'http://localhost',
+        changeOrigin: true
+    }))
+
+};
